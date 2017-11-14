00c3f7a24a226054650897375cd5319ab5e0bbce
ys <644609238@qq.com>
2017-11-14 16:50:47 +0800

    完成详情页tab切换及放大镜效果

--- src/js/details.js
+++ src/js/details.js
@@ -2,4 +2,25 @@ jQuery(function($){
     $('.getBar').load('../gonggong.html .blackBar' );
     $('.getHeader').load('../gonggong.html .headerGG' );
     $('.getFooter').load('../gonggong.html .footerGG' );
-})
+
+    // 实现点击小图切换对应的大图
+    var $sli = $('.smaller li');
+    var $bli = $('.bigger li');
+    // 隐藏所有的.content
+    
+    
+    $bli.slice(1).hide();
+  
+    $sli.first().addClass('gaoliang');
+
+    // 点击标签切换
+    $sli.on('mouseenter',function(){
+        $(this).addClass('gaoliang').siblings().removeClass('gaoliang');
+        var idx = $(this).index();
+        $bli.eq(idx).show().siblings('li').hide();
+    })
+
+
+    // 放大镜(第一张图片)
+    $bli.gdsZoom();
+});
