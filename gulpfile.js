5514d5ae878749f6fc0aaecf4f1fe51ec3e0a412
ys <644609238@qq.com>
2017-11-15 13:23:02 +0800

    购物车架构完成

--- gulpfile.js
+++ gulpfile.js
@@ -59,10 +59,21 @@ gulp.task('detailsSass',function(){
     .pipe(gulp.dest('src/css/')) //输出到硬盘
 })
 
+// 创建一个任务用来编译sass---cart
+gulp.task('cartSass',function(){
+    // 查找文件位置
+    gulp.src('./src/sass/cart.scss') //得到文件流（文件在内存中的状态）
+
+
+    .pipe(sass({outputStyle:'expanded'}).on('error',sass.logError))  //编译sass文件
+
+    .pipe(gulp.dest('src/css/')) //输出到硬盘
+})
+
 // 创建一个任务用来监听文件
 gulp.task('jtSass',function(){
     // 监听home.sass文件，如果有修改则执行compileSass任务
-    gulp.watch('./src/sass/*.scss',['signinSass','indexSass','gonggongSass','goodslistSass','detailsSass'])
+    gulp.watch('./src/sass/*.scss',['signinSass','indexSass','gonggongSass','goodslistSass','detailsSass','cartSass'])
 })
 
 
